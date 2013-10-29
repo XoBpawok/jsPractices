f9592bbbaf64b7a8e403c5e44c670ac5870ba540
XoBpawok <syslik13@gmail.com>
2013-10-29 09:43:16 +0200

    removed temp.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,4 +1,4 @@
-(function($) {
+;(function($) {
 
 	var defaultOptions = {
 		'content': 'tooltip default message',
@@ -9,7 +9,7 @@
 		'contentClassName': 'contentClassName',
 
 		'width': '200px',
-		'heigth': '20px',
+		'heigth': '200px',
 
 		'event': 'click',
 
@@ -21,67 +21,63 @@
 		}
 	}
 
-	function _createTooltipObj (options) {
+		function _createTooltipObj(options) {
 
-		var tooltip = $('<div>', {
-			'class': options.contentClassName,
-			'text': options['content']
-		});
-		tooltip.css({
+			var tooltip = $('<div>', {
+				'class': options.contentClassName,
+				'text': options['content'] || $(options.contentSelector).html()
+			});
+			tooltip.css({
 				'position': 'absolute',
 				'display': 'none',
 				'border': '1px solid #000',
 				'background': 'yellow'
 			});
-		_settooltipPosition(options, tooltip);
-		return tooltip;
-	}
-
-	function _onOpen (options) {
-		$(this).off(options.event);
-		$(this).on('mouseleave', this,	function () {
-			$('.' + options.contentClassName, this).css('display', 'none');
-			_onClose.call(this, options);
-		});
-		options['onOpen']();
-	}
-
-	function _onClose (options) {
-		$(this).off('mouseleave');
-		$(this).on(options.event, this,	function () {
-			$('.' + options.contentClassName, this).css('display', 'block');
-			_onOpen.call(this, options);
-		})
-		options['onClose']();
-	}	
-
-	function _settooltipPosition (options, tooltipObj) {
-		switch (options.position) {
-			case 'top':
-				tooltipObj.css('bottom', '100%');
-				break;
-			case 'bottom':
-				tooltipObj.css('top', '100%');
-				break;
-			case 'left':
-				tooltipObj.css('right', '100%');
-				break;
-			case 'right':
-				tooltipObj.css('left', '100%');
-				break;
-			default:
-				console.log('Error. Wrong position parameter.')
+			_settooltipPosition(options, tooltip);
+			return tooltip;
 		}
-	}
+
+		function _onOpen(options, e) {
+			$(this).off(options.event);
+			$(this).on('mouseleave', function(e) {
+				$('.' + options.contentClassName, this).css('display', 'none');
+				_onClose.call(this, options, e);
+			});
+			options['onOpen'].call();
+		}
+
+		function _onClose(options, e) {
+			$(this).off('mouseleave');
+			$(this).on(options.event, function(e) {
+				$('.' + options.contentClassName, this).css('display', 'block');
+				_onOpen.call(this, options, e);
+			})
+			options['onClose'].call();
+		}
+
+		function _settooltipPosition(options, tooltipObj) {
+			switch (options.position) {
+				case 'top':
+					tooltipObj.css('bottom', '100%');
+					break;
+				case 'bottom':
+					tooltipObj.css('top', '100%');
+					break;
+				case 'left':
+					tooltipObj.css('right', '100%');
+					break;
+				case 'right':
+					tooltipObj.css('left', '100%');
+					break;
+				default:
+					console.log('Error. Wrong position parameter.')
+			}
+		}
 
 	$.fn.tooltip = function(options) {
 		var position = this.css('position');
 
-		if (options === undefined) {
-			options = options || defaultOptions;
-		} else {
-			options = $.extend(defaultOptions, options);
-		}
+		options = $.extend(defaultOptions, options);
 
 		this.tooltipObj = this.tooltipObj || _createtooltipObj(options);
 
@@ -91,9 +87,9 @@
 
 		this.append(this.tooltipObj);
 
-		this.on(options['event'], this,	function () {
+		this.on(options['event'], this, function(e) {
 			$('.' + options.contentClassName, this).css('display', 'block');
-			_onOpen.call(this, options);
+			_onOpen.call(this, options, e);
 		});
 
 		return this;
