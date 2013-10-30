7bd414c8bb474f8ec065d7f494793219abf6aeff
XoBpawok <syslik13@gmail.com>
2013-10-30 09:51:18 +0200

    Added nodeJS files

--- js/node/server.js
+++ js/node/server.js
@@ -3,14 +3,10 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+  	var pathname = url.parse(request.url).pathname;
+  	console.log("About to route a request for " + pathname);
 
-    route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+  	route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
