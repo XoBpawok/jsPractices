3a5e8729fed087af6b144c799338a329c297a556
XoBpawok <syslik13@gmail.com>
2013-10-31 12:23:29 +0200

    completed book's examples

--- js/node/server.js
+++ js/node/server.js
@@ -2,15 +2,14 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-  	var pathname = url.parse(request.url).pathname;
-  	console.log("About to route a request for " + pathname);
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response, request);
+	}
 
-  	route(handle, pathname, response);
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
 
 exports.start = start;
